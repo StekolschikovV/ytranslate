fed5ec831dce423abed30218c888eef23c2b9ecc
Vetal <stekolschikov@mail.ua>
2016-09-07 13:59:35 +0300

    2

--- app/src/main/java/info/stekolschikov/ytranslate/YandexTranslate.java
+++ app/src/main/java/info/stekolschikov/ytranslate/YandexTranslate.java
@@ -26,29 +26,7 @@ public class YandexTranslate {
     public String retStr(){
         return strTranslate;
     }
-//    public void translate() throws IOException {
-//        String lang = "en";
-//        String input = "hi";
-//        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
-//        URL urlObj = new URL(urlStr);
-//        HttpsURLConnection connection = (HttpsURLConnection)urlObj.openConnection();
-//        connection.setRequestMethod("POST");
-//        connection.setDoOutput(true);
-//        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-//        dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);
-//
-//        InputStream response = connection.getInputStream();
-//        String json = new java.util.Scanner(response).nextLine();
-//        int start = json.indexOf("[");
-//        int end = json.indexOf("]");
-//        String translated = json.substring(start + 2, end - 1);
-////        i++;
-////        if (translated.equals(input) && i < 2) {
-////            return translate("en", input);
-////        } else return translated;
-//
-//        this.strTranslate = translated;
-//    }
+
     public String translate(String lang, String input) throws IOException {
         String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
         URL urlObj = new URL(urlStr);
